36871e81f0d101154aac4b11da3dc03d237793e7
xTeKc <bcd3v@pm.me>
2021-07-18 13:39:02 -0400

    modify imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.css'
+import App from './components/App'
+import configureStore from './store/configureStore'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
