a8ed6e571407c21017527532fcfb4a96caff223b
xTeKc <bcd3v@pm.me>
2021-07-18 13:57:18 -0400

    add openzep imports

--- src/backend/contracts/NFT.sol
+++ src/backend/contracts/NFT.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFT {
-  constructor() public {
+  constructor() {
   }
 }
